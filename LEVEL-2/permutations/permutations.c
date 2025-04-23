05c192de42c3895142244be2d1a60bb2a58dde50
ELamraniG <137711176+ELamraniG@users.noreply.github.com>
2025-04-23 23:24:12 +0100

    Update permutations.c

--- LEVEL-2/permutations/permutations.c
+++ LEVEL-2/permutations/permutations.c
@@ -1,12 +1,67 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   permutations.c                                     :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: mlaffita <marvin@42lausanne.ch>            +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/04/01 13:12:17 by mlaffita          #+#    #+#             */
-/*   Updated: 2025/04/01 13:12:18 by mlaffita         ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
+#include <unistd.h>
+
+void sort_str(char *str)
+{
+    int i, j;
+    char temp;
+
+    i = 0;
+    while (str[i])
+    {
+        j = i + 1;
+        while (str[j])
+        {
+            if (str[i] > str[j])
+            {
+                temp = str[i];
+                str[i] = str[j];
+                str[j] = temp;
+            }
+            j++;
+        }
+        i++;
+    }
+}
+#include <stdio.h>
+
+
+ int ft_strlen(char *str)
+ {
+    int i = 0;
+    if(!str)
+        return 0;
+    while (str[i])
+        i++;
+    return i;}
+void permutation(char *str,int index, int size)
+{
+    if (index == size)
+        printf("%s\n",str);
+    int i = index;
+    while (i < size)
+    {
+        char c = str[i];
+        str[i] = str[index];
+        str[index] = c;
+        permutation(str,index + 1, size);
+         c = str[i];
+        str[i] = str[index];
+        str[index] = c;
+        i++;
+    }
+}
+
+
+int main(int ac, char **av)
+{
+    if (ac != 2)
+    {
+        write(1, "\n", 1);
+        return 0;
+    }
+    char *str = av[1];
+    int size = ft_strlen(str);
+    sort_str(str);
+    permutation(str, 0, size);
+}
 
