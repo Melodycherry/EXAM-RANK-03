9342c998708d1d5bf5c181ca76d212cdc7ae29dc
ELamraniG <137711176+ELamraniG@users.noreply.github.com>
2025-04-24 15:24:12 +0100

    Update rip.c

--- LEVEL-2/rip/rip.c
+++ LEVEL-2/rip/rip.c
@@ -1,12 +1,78 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   rip.c                                              :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: mlaffita <marvin@42lausanne.ch>            +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/04/01 13:13:12 by mlaffita          #+#    #+#             */
-/*   Updated: 2025/04/01 13:13:13 by mlaffita         ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
 
+
+int	calc_min(char *str)
+{
+	int	i;
+	int	res;
+
+	i = 0;
+	res = 0;
+	while (str[i])
+	{
+		if (str[i] == '(')
+			res++;
+		if (str[i] == ')')
+			res--;
+		i++;
+	}
+	if (res < 0)
+		res = res * -1;
+	return (res);
+}
+
+int	is_valid(char *str)
+{
+	int	i;
+	int	res;
+
+	i = 0;
+	res = 0;
+	while (str[i])
+	{
+		if (str[i] == '(')
+			res++;
+		if (str[i] == ')')
+			res--;
+		if (res < 0)
+			return (0);
+		i++;
+	}
+	if (res == 0)
+		return (1);
+	return (0);
+}
+
+void	rip(char *str, int min, int index, int change, int size)
+{
+	int		i;
+	char	c;
+  if (change > min)
+      return ;
+		if (is_valid(str) && change == min)
+			puts(str);
+	i = index;
+	while (str[i])
+	{
+		c = str[i];
+		str[i] = '_';
+		change++;
+		rip(str, min, i + 1, change, size);
+		str[i] = c;
+		change--;
+		i++;
+	}
+}
+
+int	main(int ac, char **av)
+{
+	if (ac != 2 || av[1][0] == 0)
+	{
+		write (1,"\n",1);
+		return (1);
+	}
+	int min = calc_min(av[1]);
+	rip(av[1], min, 0, 0, strlen(av[1]));
+}
