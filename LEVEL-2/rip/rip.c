0717df854543226b48031cc45160683045ebe6c9
ELamraniG <137711176+ELamraniG@users.noreply.github.com>
2025-04-25 14:18:20 +0100

    Update rip.c

--- LEVEL-2/rip/rip.c
+++ LEVEL-2/rip/rip.c
@@ -1,27 +1,7 @@
 #include <stdio.h>
 #include <string.h>
 #include <unistd.h>
-
-
-int	calc_min(char *str)
-{
-	int	i;
-	int	res;
-
-	i = 0;
-	res = 0;
-	while (str[i])
-	{
-		if (str[i] == '(')
-			res++;
-		if (str[i] == ')')
-			res--;
-		i++;
-	}
-	if (res < 0)
-		res = res * -1;
-	return (res);
-}
+// don't forget to print space instead of _ ana i used _ for clarity wsalam
 
 int	is_valid(char *str)
 {
@@ -45,7 +25,32 @@ int	is_valid(char *str)
 	return (0);
 }
 
-void	rip(char *str, int min, int index, int change, int size)
+void	calc_min(char *str, int *min, int index, int change)
+{
+	int		i;
+	char	c;
+  if (change > *min)
+      return ;
+		if (is_valid(str) && change < *min)
+		{
+			*min = change;
+		}
+	i = index;
+	while (str[i])
+	{
+		c = str[i];
+		str[i] = '_';
+		change++;
+		calc_min(str, min, i + 1, change);
+		str[i] = c;
+		change--;
+		i++;
+	}
+}
+
+
+
+void	rip(char *str, int min, int index, int change)
 {
 	int		i;
 	char	c;
@@ -59,7 +64,7 @@ void	rip(char *str, int min, int index, int change, int size)
 		c = str[i];
 		str[i] = '_';
 		change++;
-		rip(str, min, i + 1, change, size);
+		rip(str, min, i + 1, change);
 		str[i] = c;
 		change--;
 		i++;
@@ -73,6 +78,7 @@ int	main(int ac, char **av)
 		write (1,"\n",1);
 		return (1);
 	}
-	int min = calc_min(av[1]);
-	rip(av[1], min, 0, 0, strlen(av[1]));
+	int min = strlen(av[1]);
+	calc_min(av[1], &min, 0, 0);
+	rip(av[1], min, 0, 0);
 }
